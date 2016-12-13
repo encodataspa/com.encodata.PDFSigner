65797c9cec4d137a7dd7b2d25472090adb85fef5
encodataspa <vito.capezzera@encodata.it>
2016-12-13 14:50:21 +0100

    Add files via upload

---
+++ www/PDFSigner.js
@@ -0,0 +1,14 @@
+var exec = require('cordova/exec');
+
+function PDFSigner() { 
+}
+
+PDFSigner.prototype.create = function(inputFile, imagePath, outputFile, x, y, width, height, successCallback, errorCallback) {
+    exec(successCallback,
+         errorCallback,
+         "PDFSigner",
+         "createPDFFromImage",
+         [inputFile, imagePath, outputFile, x, y, width, height]);
+};
+
+module.exports = new PDFSigner();
