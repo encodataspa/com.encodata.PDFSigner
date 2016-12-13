3928ff3271a72fa93e0ef76c4742725208c23d0c
encodataspa <vito.capezzera@encodata.it>
2016-12-13 11:07:03 +0100

    Update PDFSigner.java

--- src/android/PDFSigner.java
+++ src/android/PDFSigner.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pdfsigner;
+package com.encodata.PDFSigner;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
